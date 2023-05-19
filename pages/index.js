0256d041b52ef67bc5bdecb9d5427ad3c72ddab1
Adam Marsala <adammarsala@hotmail.com>
2023-05-19 12:17:51 -0400

    Added page title and header title

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pokémechanics</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Pokémechanics
         </h1>
 
         <p className={styles.description}>
