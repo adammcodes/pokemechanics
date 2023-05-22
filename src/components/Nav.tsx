138dd3b578429955a6acd3d76a69d6baa16d84f9
Adam Marsala <adammarsala@hotmail.com>
2023-05-22 13:45:36 -0400

    Add pointer arrow on hover for nav items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,15 +7,19 @@ export default function Nav() {
       <ul className={styles.nav__list}>
         <li>
           <Link href="/pokedex">Pokédex</Link>
+          <i></i>
         </li>
         <li>
           <Link href="/moves">Moves</Link>
+          <i></i>
         </li>
         <li>
           <Link href="/items">Items</Link>
+          <i></i>
         </li>
         <li>
           <Link href="/locations">Locations</Link>
+          <i></i>
         </li>
       </ul>
     </nav>
