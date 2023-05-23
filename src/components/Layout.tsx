76240a201e6f3bf81857d9902d7ca1a8e2ab740f
Adam Marsala <adammarsala@hotmail.com>
2023-05-23 17:08:10 -0400

    remove commented code for changing font in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,3 @@
-// import { useContext, useEffect } from "react";
-// import { GameContext } from "../context/_context";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "../../styles/Home.module.css";
@@ -10,20 +8,7 @@ type LayoutProps = {
 };
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  // Get currently selected game for it's version url
-  // const { font, setFont } = useContext(GameContext);
-
-  // let fontIndex = font;
   let fontIndex = 2;
-
-  // useEffect(() => {
-  //   const storedFont = localStorage.getItem("font");
-  //   if (storedFont) {
-  //     const font = JSON.parse(storedFont);
-  //     setFont(font);
-  //     fontIndex = font;
-  //   }
-  // }, []);
 
   return (
     <div className={`${styles.container}`}>
@@ -35,9 +20,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
           * {
             font-size: ${fontIndex >= 2 ? 1.5 : 1}em;
             line-height: 1em;
-            font-family: ${fonts[fontIndex]
-              ? fonts[fontIndex].style.fontFamily
-              : fonts[0].style.fontFamily};
+            font-family: ${fonts[fontIndex].style.fontFamily};
           }
         `}
       </style>
