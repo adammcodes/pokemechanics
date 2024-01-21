9d63afca7006ed53f004439aba392dca2d1cf0ce
Adam Marsala <adammarsala@hotmail.com>
2024-01-20 20:58:17 -0500

    Add animation delay to typing

--- app/page.tsx
+++ app/page.tsx
@@ -9,38 +9,38 @@ type Gen = {
   url: string;
 };
 
-async function getVersionGroups(): Promise<Gen[]> {
-  const api = new GameClient();
-  return await api
-    .listVersionGroups(0, 25) // right now supporting up to scarlet-violet
-    .then((data: NamedAPIResourceList) => {
-      return data.results;
-    })
-    .catch((error: any) => error);
-}
+// async function getVersionGroups(): Promise<Gen[]> {
+//   const api = new GameClient();
+//   return await api
+//     .listVersionGroups(0, 25) // right now supporting up to scarlet-violet
+//     .then((data: NamedAPIResourceList) => {
+//       return data.results;
+//     })
+//     .catch((error: any) => error);
+// }
 
 // This is the "/" route
 export default async function HomePage() {
-  const gens = await getVersionGroups();
+  // const gens = await getVersionGroups();
 
-  // if there is an error, show an error page
-  if (gens instanceof Error) {
-    return (
-      <main>
-        <h1>There was an error</h1>
-        <p>{gens.message}</p>
-      </main>
-    );
-  }
+  // // if there is an error, show an error page
+  // if (gens instanceof Error) {
+  //   return (
+  //     <main>
+  //       <h1>There was an error</h1>
+  //       <p>{gens.message}</p>
+  //     </main>
+  //   );
+  // }
 
   return (
     <main className="w-full">
       <h2 className={styles.typing}>
         WHICH GAME ARE YOU PLAYING?<span className={styles.cursor}>_</span>
       </h2>
-      <div className="max-w-sm mx-auto">
+      {/* <div className="max-w-sm mx-auto">
         {gens && <GenSelector gens={gens} />}
-      </div>
+      </div> */}
     </main>
   );
 }
