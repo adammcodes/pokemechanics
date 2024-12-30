edac25e4fa990fe52faa8627d2c14c8472f98f85
Adam Marsala <adammarsala@hotmail.com>
2024-12-29 23:43:08 -0500

    Update year in footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      POKEMON AND ALL RESPECTIVE NAMES ARE TRADEMARK AND © OF NINTENDO 1996-2023
+      POKEMON AND ALL RESPECTIVE NAMES ARE TRADEMARK AND © OF NINTENDO 1996-2025
     </footer>
   );
 }
