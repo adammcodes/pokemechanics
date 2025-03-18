6733d1073f4ef3223d8b0f833426393fdcb98673
Adam Marsala <adammarsala@hotmail.com>
2025-03-17 23:39:43 -0400

    Make the Box component scrollable after a certain height

--- src/components/common/Box.tsx
+++ src/components/common/Box.tsx
@@ -7,7 +7,7 @@ export default function Box({
 }) {
   return (
     <div
-      className={`card__border h-full p-3 flex flex-col gap-y-3 w-full lg:w-[300px]`}
+      className={`card__border h-full p-3 flex flex-col gap-y-3 w-full lg:w-[300px] max-h-[425px] overflow-y-auto`}
     >
       <h2 className="text-2xl leading-none">{headingText}</h2>
       {children}
