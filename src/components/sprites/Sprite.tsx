14f5b039e029b5fe4ef17790b399338abd3cceba
Adam Marsala <adammarsala@hotmail.com>
2025-10-02 00:37:05 -0400

    Remove unused client side code

--- src/components/sprites/Sprite.tsx
+++ src/components/sprites/Sprite.tsx
@@ -1,7 +1,4 @@
-import { useQuery } from "react-query";
 import DynamicImage from "../common/DynamicImage";
-import usePokemonClient from "@/hooks/usePokemonClient";
-import getSpriteUrl from "@/constants/spriteUrlTemplates";
 import getSpriteIconUrl from "@/constants/spriteIconUrlTemplate";
 
 type SpriteProps = {
@@ -12,24 +9,6 @@ type SpriteProps = {
 };
 
 export const Sprite: React.FC<SpriteProps> = ({ id, size, gen, game }) => {
-  // const api = usePokemonClient();
-  // const p = useQuery(
-  //   ["pokemonSprite", id],
-  //   async () => {
-  //     return api
-  //       .getPokemonById(Number(id))
-  //       .then((data) => data)
-  //       .catch((err) => {
-  //         throw err;
-  //       });
-  //   },
-  //   {
-  //     refetchOnMount: false,
-  //     refetchOnWindowFocus: false,
-  //     enabled: Boolean(id),
-  //   }
-  // );
-
   const spriteIconSrc = getSpriteIconUrl({
     pokemonId: id,
     generation: gen,
