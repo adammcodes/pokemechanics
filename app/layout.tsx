c957e72574cc4dd4166ad64c0ff80a91f02c999e
Adam Marsala <adammarsala@hotmail.com>
2025-10-02 18:50:29 -0400

    Add manifest and app logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,21 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://raw.githubusercontent.com" />
         <link rel="dns-prefetch" href="https://raw.githubusercontent.com" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Pokémechanics" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Client initialGame={game}>{children}</Client>
