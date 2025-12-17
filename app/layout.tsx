2db94d39491e875cac7160c4894d36129b7c34f6
Adam Marsala <adammarsala@hotmail.com>
2025-12-16 19:41:49 -0500

    fix warning in console

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           href="/apple-touch-icon.png"
         />
         <meta name="apple-mobile-web-app-title" content="Pokémechanics" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
